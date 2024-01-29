79d066eea2b8d71f9b1dc8a6eb523fbe5cefe1a4
Lee Jinsol <jins2ol@naver.com>
2024-01-29 16:58:17 +0900

    Feat/production 환경에서 콘솔 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,12 @@ function App() {
     init();
   }, []);
 
+  if (process.env.NODE_ENV === "production") {
+    console.log = function no_console() {};
+    console.warn = function no_console() {};
+    console.error = function no_console() {};
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
