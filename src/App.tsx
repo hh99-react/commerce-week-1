ce9765347742ed5cfcd95927963a9602d4c570d6
1eejinsol <130551771+jinsollee2023@users.noreply.github.com>
2024-01-29 17:19:07 +0900

    Fix/사용하지 않는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,6 @@ function App() {
     init();
   }, []);
 
-  if (process.env.NODE_ENV === "production") {
-    console.log = function no_console() {};
-    console.warn = function no_console() {};
-    console.error = function no_console() {};
-  }
-
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
